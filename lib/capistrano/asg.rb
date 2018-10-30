c5e2be791cb8ee1dbe463fda2c901bf0782959d4
Gene Hand <ghand@doximity.com>
2018-10-29 17:36:26 -0700

    Add :partial_roles to run on a subset of instances in the ASG

--- lib/capistrano/asg.rb
+++ lib/capistrano/asg.rb
@@ -24,7 +24,7 @@ require 'capistrano/dsl'
 
 load File.expand_path('../asg/tasks/asg.rake', __FILE__)
 
-def autoscale(groupname, *args)
+def autoscale(groupname, **args)
   include Capistrano::DSL
   include Capistrano::Asg::Aws::AutoScaling
   include Capistrano::Asg::Aws::EC2
@@ -38,6 +38,16 @@ def autoscale(groupname, *args)
   (regions[region] ||= []) << groupname
   set :regions, regions
 
+  # Create an array of role names to be distributed across the ASG
+  partial_queue = []
+  if args.key?(:partial_roles)
+    args[:partial_roles].each do |partial|
+      instances = partial.key?(:instances) ? partial[:instances] : 1
+      instances.times { partial_queue << partial[:name].to_s }
+    end
+    args.delete(:partial_roles)
+  end
+
   asg_instances.each do |asg_instance|
     if asg_instance.health_status != 'Healthy'
       puts "Autoscaling: Skipping unhealthy instance #{asg_instance.id}"
@@ -45,15 +55,24 @@ def autoscale(groupname, *args)
       ec2_instance = ec2_resource.instance(asg_instance.id)
       hostname = ec2_instance.private_ip_address
       puts "Autoscaling: Adding server #{hostname}"
-      server(hostname, *args)
+      # create a complete temp copy of the array contents instead of just copying the references
+      host_args = Marshal.load(Marshal.dump(args))
+      if additional_role = partial_queue.shift
+        host_args[:roles] << additional_role
+      end
+      server(hostname, **host_args)
     end
   end
 
-  if asg_instances.count > 0 && fetch(:create_ami, true)
-    after('deploy:finishing', 'asg:scale')
-  else
-    puts 'Autoscaling: AMI could not be created because no running instances were found.\
-      Is your autoscale group name correct?'
+  puts "WARNING: Not all partial roles were assigned: #{partial_queue}" unless partial_queue.empty?
+
+  if fetch(:create_ami, true)
+    if asg_instances.count > 0
+      after('deploy:finishing', 'asg:scale')
+    else
+      puts 'Autoscaling: AMI could not be created because no running instances were found.\
+        Is your autoscale group name correct?'
+    end
   end
 
   reset_autoscaling_objects
